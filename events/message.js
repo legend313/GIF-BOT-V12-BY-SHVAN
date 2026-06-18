91d8cc6b95d68e1d3dc571200bb0ee3a7f3acdb5
legend313 <86923699+legend313@users.noreply.github.com>
2026-06-18 22:00:00 +0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,29 +1,3 @@
-const config = require('../config.json');
-
-module.exports = message => {
-
+module.exports = async (client, message) => {
   if (message.author.bot) return;
-
-  if (!message.content.startsWith(config.prefix)) return;
-
-  const client = message.client;
-
-  let command = message.content
-    .split(' ')[0]
-    .slice(config.prefix.length)
-    .toLowerCase();
-
-  let params = message.content.split(' ').slice(1);
-
-  let cmd;
-
-  if (client.commands.has(command)) {
-    cmd = client.commands.get(command);
-  } else if (client.aliases.has(command)) {
-    cmd = client.commands.get(client.aliases.get(command));
-  }
-
-  if (cmd) {
-    cmd.run(client, message, params);
-  }
 };
