ded1113f8fd629cab890fa63c5f2fe971289de35
legend313 <86923699+legend313@users.noreply.github.com>
2026-06-18 17:11:36 +0300

    Update shvan.js

--- shvan.js
+++ shvan.js
@@ -1,133 +1,41 @@
-const discord = require('discord.js');
-const fs = require('fs');
-const http = require('http');
-const express = require('express');
-const config = require('./config.json');
-const app = express();
-const Discord = require('discord.js');
-const client = new Discord.Client();
-client.on('ready', async () => {
-   client.appInfo = await client.fetchApplication();
-  setInterval( async () => {
-    client.appInfo = await client.fetchApplication();
-  }, 600);
-  
- client.user.setActivity(`=help`, { type:"LISTENING" })
-  console.log("${client.user.tag}")
-});
+const Discord = require("discord.js");
+const fs = require("fs");
+const config = require("./config.json");
 
-const log = message => {
-  console.log(` ${message}`);
-};
-require('./util/eventLoader.js')(client);
+const client = new Discord.Client();
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
-fs.readdir('./commandgif/', (err, files) => {
-    if (err) console.error(err);
-    log(`${files.length} komut yüklenecek.`);
-    files.forEach(f => {
-        let props = require(`./commandgif/${f}`);
-        log(`Yüklenen komut: ${props.help.name}.`);
-        client.commands.set(props.help.name, props);
-        props.conf.aliases.forEach(alias => {
-            client.aliases.set(alias, props.help.name);
-        });
+
+console.log("Bot is starting...");
+
+// Load commands
+fs.readdir("./commandgif/", (err, files) => {
+  if (err) console.error(err);
+
+  files.forEach(file => {
+    let props = require(`./commandgif/${file}`);
+
+    console.log(`Loaded command: ${props.help.name}`);
+
+    client.commands.set(props.help.name, props);
+
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
     });
+  });
 });
 
+// Load events
+require("./util/eventLoader")(client);
 
+client.on("ready", () => {
+  console.log(`${client.user.tag} is online!`);
 
+  client.user.setActivity(`${config.prefix}help`, {
+    type: "LISTENING"
+  });
+});
 
-client.reload = command => {
-    return new Promise((resolve, reject) => {
-        try {
-            delete require.cache[require.resolve(`./commandgif/${command}`)];
-            let cmd = require(`./commandgif/${command}`);
-            client.commands.delete(command);
-            client.aliases.forEach((cmd, alias) => {
-                if (cmd === command) client.aliases.delete(alias);
-            });
-            client.commands.set(command, cmd);
-            cmd.conf.aliases.forEach(alias => {
-                client.aliases.set(alias, cmd.help.name);
-            });
-            resolve();
-        } catch (e) {
-           reject(e);
-        }
-    });
-};
-
-client.load = command => {
-    return new Promise((resolve, reject) => {
-        try {
-            let cmd = require(`./commandgif/${command}`);
-            client.commands.set(command, cmd);
-            cmd.conf.aliases.forEach(alias => {
-                client.aliases.set(alias, cmd.help.name);
-            });
-            resolve();
-        } catch (e) {
-            reject(e);
-        }
-    });
-};
-
-
-
-
-client.unload = command => {
-    return new Promise((resolve, reject) => {
-        try {
-            delete require.cache[require.resolve(`./commandgif/${command}`)];
-            let cmd = require(`./commandgif/${command}`);
-            client.commands.delete(command);
-            client.aliases.forEach((cmd, alias) => {
-                if (cmd === command) client.aliases.delete(alias);
-            });
-            resolve();
-        } catch (e) {
-            reject(e);
-        }
-    });
-};
-
-
-client.elevation = message => {
-    if (!message.guild) {
-        return;
-    }
-    let permlvl = 0;
-    if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
-    if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
-    if (message.author.id === config.sahip) permlvl = 4;
-    return permlvl;
-};
-client.login(config.token)
-
-
-//------------------------ Halka Açık Random Gif Pp -----------------------//
-//------------------------ Halka Açık Random Gif Pp -----------------------//
-//------------------------ Halka Açık Random Gif Pp -----------------------//
-
-client.on("userUpdate", async(Revenge, NYKS) => {
-  
-if(Revenge.avatarURL === NYKS.avatarURL) return;
-let RevengePP = NYKS.avatarURL.split("?")[0]
-
-const Kanal = client.channels.filter(Revenge => Revenge.name === 'random-gif' && Revenge.type === 'text').array().forEach(Nucceteere => {
-if (RevengePP.endsWith('gif')) {
-Nucceteere.send(new Discord.Attachment(RevengePP))
-}
-const Kanal2 = client.channels.filter(Revenge => Revenge.name === 'random-pp' && Revenge.type === 'text').array().forEach(Revenge => {
-if (!RevengePP.endsWith('gif')) {
-Revenge.send(new Discord.Attachment(RevengePP))
-}
-})
-})
-})
-
-//------------------------ Halka Açık Random Gif Pp -----------------------//
-//------------------------ Halka Açık Random Gif Pp -----------------------//
-//------------------------ Halka Açık Random Gif Pp -----------------------//
+// Login bot
+client.login(config.token);
