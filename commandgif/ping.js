82c656f2e830d90c5f2e7f62a73c5b5188bba860
legend313 <86923699+legend313@users.noreply.github.com>
2026-06-18 22:00:15 +0300

    Update ping.js

--- commandgif/ping.js
+++ commandgif/ping.js
@@ -1,40 +1,6 @@
-const Discord = require('discord.js');
-const db = require('quick.db');
-
-exports.run = async (app, message, client) => {
-  
-const darknes = new Discord.MessageEmbed()
-
-.setColor("RANDOM")
-.setDescription('Ping')
-
-let start = Date.now(); 
-let mesaj = await message.channel.send(darknes)
-let diff = (Date.now() - start); 
-let API = (app.ws.ping).toFixed(2)
-    
-setInterval(() => {
-        
-const only = new Discord.MessageEmbed()
-
-.setDescription(`**${diff}ms**
-**${API}ms**`)
-
-mesaj.edit(only);
-      
-}, 5000)
-  
-};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0
-};
-
-exports.help = {
-  name: 'ping',
-  description: 'rexuss',
-  usage: 'ping'
+module.exports = {
+  name: "ping",
+  run: async (client, message) => {
+    message.reply(`🏓 ${client.ws.ping}ms`);
+  }
 };
